3c37fa5255586cd9819a27918267f7772e5e9250
Derlie Monne <derlie.monne@gmail.com>
2023-06-06 10:56:32 +1000

    Update game.rs

--- src/engine/game.rs
+++ src/engine/game.rs
@@ -5,15 +5,17 @@ use crate::math::*;
 pub struct Game<'a> {
     pub coordinate_system: CoordinateSystem,
     pub entities: Vec<Entity>,
-    pub systems: Vec<&'a dyn Fn(&Game)>
+    pub systems: Vec<&'a dyn Fn(&Game)>,
+    pub config: GameConfig,
 }
 
 impl<'a> Game<'a> {
-    pub fn new(coordinate_system: CoordinateSystem) -> Game<'a> {
+    pub fn new(coordinate_system: CoordinateSystem, config: GameConfig) -> Game<'a> {
         Game { 
             coordinate_system, 
             entities: vec![],
             systems: vec![],
+            config,
         }
     }
 
